4d8dfdb0ef88138a7d56b89cb3321b292975ab40
Roberto Gasparotto <software@daint.it>
2021-04-09 17:26:26 +0200

    login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IloginResult } from 'src/app/data/loginResult';
+import { AuthService } from 'src/app/services/auth.service';
+import {login} from '../../data/login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username: string
+  password: string
+  constructor(private srv: AuthService) { }
 
   ngOnInit() {
   }
 
+  async onLogin(){
+    let lg: login;
+    let logged: IloginResult;
+    lg = new login();    
+    lg.userName = this.username;
+    lg.password = this.password;
+    logged = await this.srv.log(lg);
+  }
 }
